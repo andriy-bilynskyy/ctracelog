8733f507ba1fe27702a202da8f27393ec5bf7b9b
Andriy <andriy.bilynskyy@gmail.com>
2018-05-05 21:36:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ctracelog
+## Description
+Simple trace logger implemented on c
+## Features
+- small size
+- different logging levels (INFO, DEBUG, WARNING, ERROR, CRITICAL)
+- selectable tracing by levels
+- diffeent output devices
+- thread safe
+- show buffers in hex
+- tracable assert
---
+++ src/main.c
@@ -0,0 +1,40 @@
+#include "trace_log.h"
+#include <stdint.h>
+
+
+int main(void)
+{
+
+    trace_log_setlevel(LOG_INFO);
+
+    trace_log_printf(LOG_INFO, "this is info");
+    trace_log_printf(LOG_DEBUG, "this is debug");
+    trace_log_printf(LOG_WARNING, "this is warning");
+    trace_log_printf(LOG_ERROR, "this is error");
+    trace_log_printf(LOG_CRITICAL, "this is critical");
+
+    trace_log_printf(LOG_DEBUG, "print parameters %d, %s, %f", 157, "test",157.0);
+
+    uint8_t buf[] = {1,2,3,4,5,6,7,8,9,0};
+    trace_log_printbuf(LOG_WARNING, buf, sizeof(buf), "small buffer of %u elements", sizeof(buf));
+
+    uint8_t bufl[100];
+    for(uint8_t i =0; i < 100; i++) bufl[i] = i;
+    trace_log_printbufl(LOG_DEBUG, bufl, sizeof(bufl), "big buffer of %u elements", sizeof(bufl));
+
+    trace_log_set_output(stderr);
+    trace_log_printf(LOG_CRITICAL, "this is printed to std error. can be used any FILE.");
+    trace_log_set_output(stderr);
+
+    trace_log_printf(LOG_INFO, "the next print will be disabled");
+    trace_log_setlevel(LOG_OFF);
+    trace_log_printf(LOG_CRITICAL, "this is suppressed message");
+    trace_log_setlevel(LOG_DEBUG);
+    trace_log_printf(LOG_DEBUG, "this is shown message");
+    trace_log_printf(LOG_INFO, "this is suppressed message");
+
+    trace_log_assert(sizeof(buf) == sizeof(bufl), LOG_CRITICAL, "this shouldn't happen!");
+    trace_log_assert(trace_log_getlevel() == LOG_DEBUG, LOG_CRITICAL, "that's fine just for test (should be not printed).");
+
+    return 0;
+}
